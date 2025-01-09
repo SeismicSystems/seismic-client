51cc9192a683c31c03906f4899b3e8035b24f4cf
cdrappi <c@seismic.systems>
2025-01-09 15:10:11 -0500

    docs

---
+++ packages/seismic-viem/vocs.config.ts
@@ -0,0 +1,9 @@
+import type { Config } from 'vocs'
+import { defineConfig } from 'vocs'
+
+const config: Config = defineConfig({
+  title: 'Seismic Viem',
+  description: 'Build Seismic apps by extending viem',
+})
+
+export default config
