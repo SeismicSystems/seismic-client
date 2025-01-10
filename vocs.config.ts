167c62425bfcfed8c35564c469d48286496d7cbd
cdrappi <c@seismic.systems>
2025-01-10 16:45:16 -0500

    does eslint work

--- vocs.config.ts
+++ vocs.config.ts
@@ -1,11 +1,6 @@
-import fs from 'fs'
-import path from 'path'
-import type { Config } from 'vocs'
 import { defineConfig } from 'vocs'
 
-const config: Config = defineConfig({
+export default defineConfig({
   title: 'Seismic Viem',
   description: 'Build Seismic apps by extending viem',
 })
-
-export default config
