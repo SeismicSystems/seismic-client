dc21ba7b7a20e361829d81607bc0274c33c4237a
cdrappi <c@seismic.systems>
2025-02-20 14:17:26 -0500

    add faucet api

---
+++ packages/seismic-viem/src/faucet.ts
@@ -0,0 +1,24 @@
+import { type Hex, type PublicClient, parseEther } from 'viem'
+
+type CheckFaucetParams = {
+  address: Hex
+  publicClient: PublicClient
+  faucetUrl: string
+}
+
+export const checkFaucet = async ({
+  address,
+  publicClient,
+  faucetUrl,
+}: CheckFaucetParams): Promise<string | null> => {
+  const balance = await publicClient.getBalance({ address })
+  if (balance > parseEther('0.5')) {
+    return null
+  }
+  const { msg } = await fetch(faucetUrl, {
+    method: 'POST',
+    body: JSON.stringify({ address }),
+  }).then((res) => res.json())
+  // returns string like 'Txhash: 0xca59c...7cf1'
+  return msg
+}
