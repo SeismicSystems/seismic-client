5f2752c146cc8a544d7513e3f78f8380cd389561
cdrappi <c@seismic.systems>
2025-03-10 12:31:30 -0400

    console og

--- packages/seismic-bot/src/faucetChecker.ts
+++ packages/seismic-bot/src/faucetChecker.ts
@@ -4,6 +4,7 @@ import { FaucetManager } from '@sbot/faucetManager'
 import SlackNotifier from '@sbot/slack'
 
 export const checkAllFaucets = async (chains: Chain[]) => {
+  console.log(`Checking all faucets across ${chains.length} chains`)
   const slack = new SlackNotifier(process.env.SLACK_TOKEN!)
   const faucetPrivateKey = process.env.FAUCET_PRIVATE_KEY! as Hex
   const faucetReservePrivateKey = process.env.FAUCET_RESERVE_PRIVATE_KEY! as Hex
