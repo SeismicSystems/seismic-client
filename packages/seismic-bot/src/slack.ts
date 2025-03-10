71fd3f6eb9471089cedba82498c5d414aeb1861a
cdrappi <c@seismic.systems>
2025-03-10 13:12:47 -0400

    better bot

--- packages/seismic-bot/src/slack.ts
+++ packages/seismic-bot/src/slack.ts
@@ -1,10 +1,20 @@
 import type {
   ChatPostMessageArguments,
+  ChatPostMessageResponse,
   MessageAttachment,
 } from '@slack/web-api'
 import { WebClient } from '@slack/web-api'
 
 const LIGHT_GRAY = '#D3D3D3'
+
+type SlackMessage = {
+  channel: string
+  message: string
+  title?: string
+  // Can be any hex color e.g. '#439FE0'
+  color?: 'good' | 'warning' | 'danger' | string
+  threadTs?: string
+} & Partial<ChatPostMessageArguments>
 
 class SlackNotifier {
   private web: WebClient
@@ -21,13 +31,8 @@ class SlackNotifier {
     message,
     title,
     color,
-  }: {
-    channel: string
-    message: string
-    title?: string
-    // Can be any hex color e.g. '#439FE0'
-    color?: 'good' | 'warning' | 'danger' | string
-  } & Partial<ChatPostMessageArguments>): Promise<any> {
+    threadTs,
+  }: SlackMessage): Promise<ChatPostMessageResponse> {
     const defaultAttachment: MessageAttachment = {
       color,
       title,
@@ -39,27 +44,40 @@ class SlackNotifier {
       attachments: [defaultAttachment],
       // @ts-expect-error: legacy slack api
       fallback: `${title}\n${message}`,
+      thread_ts: threadTs,
     }
 
     console.log(`Posting to ${channel}: ${JSON.stringify({ title, message })}`)
     return this.web.chat.postMessage(postParams)
   }
 
-  async urgent(message: string, title?: string) {
+  async urgent({
+    message,
+    title,
+    color = 'danger',
+    threadTs,
+  }: Omit<SlackMessage, 'channel'>) {
     return this.send({
       channel: '#log-urgent',
-      color: 'danger',
+      color,
       message,
       title,
+      threadTs,
     })
   }
 
-  async status(message: string, title?: string) {
+  async status({
+    message,
+    title,
+    color = LIGHT_GRAY,
+    threadTs,
+  }: Omit<SlackMessage, 'channel'>) {
     return this.send({
       channel: '#log-status',
-      color: LIGHT_GRAY,
+      color,
       message,
       title,
+      threadTs,
     })
   }
 }
