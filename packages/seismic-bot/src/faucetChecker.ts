a10eb2a06fb5350ec1d352ffdf7a4d7d473260c7
Christian Drappi <c@seismic.systems>
2025-03-10 12:57:38 -0400

    Seismic notification bot; upgrade bun lockfile (#75)

---
+++ packages/seismic-bot/src/faucetChecker.ts
@@ -0,0 +1,20 @@
+import type { Chain, Hex } from 'viem'
+
+import { FaucetManager } from '@sbot/faucetManager'
+import SlackNotifier from '@sbot/slack'
+
+export const checkAllFaucets = async (chains: Chain[]) => {
+  console.log(`Checking all faucets across ${chains.length} chains`)
+  const slack = new SlackNotifier(process.env.SLACK_TOKEN!)
+  const faucetPrivateKey = process.env.FAUCET_PRIVATE_KEY! as Hex
+  const faucetReservePrivateKey = process.env.FAUCET_RESERVE_PRIVATE_KEY! as Hex
+  for (const chain of chains) {
+    const manager = new FaucetManager(
+      chain,
+      faucetPrivateKey,
+      faucetReservePrivateKey,
+      slack
+    )
+    await manager.checkChain()
+  }
+}
---
+++ packages/seismic-bot/src/slack.ts
@@ -0,0 +1,67 @@
+import type {
+  ChatPostMessageArguments,
+  MessageAttachment,
+} from '@slack/web-api'
+import { WebClient } from '@slack/web-api'
+
+const LIGHT_GRAY = '#D3D3D3'
+
+class SlackNotifier {
+  private web: WebClient
+
+  constructor(token: string) {
+    this.web = new WebClient(token)
+  }
+
+  /**
+   * Sends a basic message to a channel with a color
+   */
+  async send({
+    channel,
+    message,
+    title,
+    color,
+  }: {
+    channel: string
+    message: string
+    title?: string
+    // Can be any hex color e.g. '#439FE0'
+    color?: 'good' | 'warning' | 'danger' | string
+  } & Partial<ChatPostMessageArguments>): Promise<any> {
+    const defaultAttachment: MessageAttachment = {
+      color,
+      title,
+      text: message,
+    }
+
+    const postParams: ChatPostMessageArguments = {
+      channel,
+      attachments: [defaultAttachment],
+      // @ts-expect-error: legacy slack api
+      fallback: `${title}\n${message}`,
+    }
+
+    console.log(`Posting to ${channel}: ${JSON.stringify({ title, message })}`)
+    return this.web.chat.postMessage(postParams)
+  }
+
+  async urgent(message: string, title?: string) {
+    return this.send({
+      channel: '#log-urgent',
+      color: 'danger',
+      message,
+      title,
+    })
+  }
+
+  async status(message: string, title?: string) {
+    return this.send({
+      channel: '#log-status',
+      color: LIGHT_GRAY,
+      message,
+      title,
+    })
+  }
+}
+
+export default SlackNotifier
