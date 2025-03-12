3558dbaf904ca4c42ae7e4da635db5c851a56a9b
cdrappi <c@seismic.systems>
2025-03-12 11:58:57 -0400

    fix typing

--- packages/seismic-viem/src/faucet.ts
+++ packages/seismic-viem/src/faucet.ts
@@ -63,8 +63,8 @@ export const checkFaucet = async ({
 
   if (msg.startsWith('Txhash: ')) {
     const hash = msg.slice(8)
-    if (hash.startsWith('0x') && hash.length === 66) {
-      const txUrl = txExplorerUrl({ publicClient, txHash: hash })
+    if (hash.startsWith('0x') && hash.length === 66 && publicClient.chain) {
+      const txUrl = txExplorerUrl({ chain: publicClient.chain, txHash: hash })
       if (txUrl) {
         console.debug(`Faucet sent eth to ${address}: ${txUrl}`)
       }
