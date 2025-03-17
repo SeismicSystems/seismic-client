87fc329793d49f624f876914f5d69869a0b5af8e
cdrappi <c@seismic.systems>
2025-03-17 11:47:57 -0400

    export all tests

---
+++ packages/seismic-viem-tests/src/tests/contract/contract.ts
@@ -0,0 +1,84 @@
+import { expect } from 'bun:test'
+
+import { contractABI } from '@sviem-tests/tests/contract/abi.ts'
+import { bytecode } from '@sviem-tests/tests/contract/bytecode.ts'
+import { ShieldedPublicClient, ShieldedWalletClient } from '@sviem/client.ts'
+import { getShieldedContract } from '@sviem/contract/contract.ts'
+import { stringifyBigInt } from '@sviem/utils.ts'
+
+type ContractTestArgs = {
+  publicClient: ShieldedPublicClient
+  walletClient: ShieldedWalletClient
+}
+
+export const testSeismicTx = async ({
+  publicClient,
+  walletClient,
+}: ContractTestArgs) => {
+  const testContractBytecodeFormatted: `0x${string}` = `0x${bytecode.object.replace(/^0x/, '')}`
+  const TEST_NUMBER = BigInt(11)
+
+  const deployTx = await walletClient.deployContract({
+    abi: contractABI,
+    bytecode: testContractBytecodeFormatted,
+    chain: walletClient.chain,
+  })
+  const deployReceipt = await publicClient.waitForTransactionReceipt({
+    hash: deployTx,
+  })
+
+  const deployedContractAddress = deployReceipt.contractAddress!
+  console.info(`Deployed contract address: ${deployedContractAddress}`)
+
+  const seismicContract = getShieldedContract({
+    abi: contractABI,
+    address: deployedContractAddress,
+    client: walletClient,
+  })
+
+  const isOdd0 = await walletClient.readContract({
+    address: deployedContractAddress,
+    abi: contractABI,
+    functionName: 'isOdd',
+  })
+  // contract initializes number to be 0
+  expect(isOdd0).toBe(false)
+  console.info(`[0] initial value of isOdd = ${isOdd0}`)
+
+  const tx1 = await seismicContract.write.setNumber([TEST_NUMBER])
+  console.info(`[1] Set number tx: ${tx1}`)
+  const receipt1 = await publicClient.waitForTransactionReceipt({ hash: tx1 })
+  console.info(
+    `[1] setNumber receipt: ${JSON.stringify(receipt1, stringifyBigInt, 2)}`
+  )
+
+  // Try reading using explicit signedRead
+  const isOdd1 = await seismicContract.read.isOdd()
+  // number has been set to 11
+  expect(isOdd1).toBe(true)
+
+  const tx2 = await seismicContract.write.increment()
+  console.info(`[2] Incremented number in tx: ${tx2}`)
+  const receipt2 = await publicClient.waitForTransactionReceipt({ hash: tx2 })
+  console.info(
+    `[2] Increment receipt: ${JSON.stringify(receipt2, stringifyBigInt, 2)}`
+  )
+
+  // Try reading using unsigned (normal) read
+  const isOdd2 = await seismicContract.tread.isOdd()
+  expect(isOdd2).toBe(false)
+
+  const tx3 = await seismicContract.write.setNumber([TEST_NUMBER])
+  console.info(`[3] Set number tx: ${tx1}`)
+  const receipt3 = await publicClient.waitForTransactionReceipt({ hash: tx3 })
+  console.info(
+    `[3] setNumber receipt: ${JSON.stringify(receipt3, stringifyBigInt, 2)}`
+  )
+
+  // Use non-explicit signed-read
+  const isOdd3 = await seismicContract.tread.isOdd([], {
+    account: walletClient.account.address,
+  })
+  // number has been set back to 11
+  expect(isOdd3).toBe(true)
+}
