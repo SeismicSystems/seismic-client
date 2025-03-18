e314481cbd8217a30af300f292ca35f603f20ced
cdrappi <c@seismic.systems>
2025-03-18 14:30:56 -0400

    proper test

--- tests/seismic-viem/src/ws.test.ts
+++ tests/seismic-viem/src/ws.test.ts
@@ -1,23 +1,34 @@
-import { describe, test } from 'bun:test'
+import { afterAll, beforeAll, describe, test } from 'bun:test'
+import { Chain } from 'viem'
 
 import { buildNode, envChain, setupNode } from '@sviem-tests/index.ts'
 import { testWsConnection } from '@sviem-tests/tests/ws.ts'
+
+let chain: Chain
+let port: number
+let exitProcess: () => Promise<void>
+
+beforeAll(async () => {
+  chain = envChain()
+  port = 8549
+  await buildNode(chain)
+  const node = await setupNode(chain, { port, ws: true })
+  exitProcess = node.exitProcess
+})
 
 describe('ws', () => {
   test(
     'should connect to the ws',
     async () => {
-      const chain = envChain()
-      const port = 8549
-      await buildNode(chain)
-      const { exitProcess } = await setupNode(chain, { port, ws: true })
-
       await testWsConnection({
         chain,
         wsUrl: `ws://localhost:${port}`,
       })
-      await exitProcess()
     },
     { timeout: 20_000 }
   )
+})
+
+afterAll(async () => {
+  await exitProcess()
 })
