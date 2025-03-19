a8db2f0d6af92838194e8028a38d1650a0088c51
Christian Drappi <c@seismic.systems>
2025-03-19 08:56:51 -0400

    Tests: make test timeout param a constant (#105)

--- tests/seismic-viem/src/viem.test.ts
+++ tests/seismic-viem/src/viem.test.ts
@@ -24,6 +24,8 @@ import {
 } from '@sviem-tests/tests/typedData.ts'
 import { testWsConnection } from '@sviem-tests/tests/ws.ts'
 
+const TIMEOUT_MS = 20_000
+
 const ENC_SK =
   '0x311d54d3bf8359c70827122a44a7b4458733adce3c51c6b59d9acfce85e07505'
 const ENC_PK =
@@ -55,7 +57,7 @@ describe('Seismic Contract', async () => {
     'deploy & call contracts with seismic tx',
     () => testSeismicTx({ chain, url, account: testAccount }),
     {
-      timeout: 20_000,
+      timeout: TIMEOUT_MS,
     }
   )
 })
@@ -72,7 +74,7 @@ describe('Seismic Transaction Encoding', async () => {
         encryptionPubkey: ENC_PK,
       }),
     {
-      timeout: 20_000,
+      timeout: TIMEOUT_MS,
     }
   )
 })
@@ -88,7 +90,7 @@ describe('Typed Data', async () => {
         encryptionSk: ENC_SK,
         encryptionPubkey: ENC_PK,
       }),
-    { timeout: 20_000 }
+    { timeout: TIMEOUT_MS }
   )
 
   test(
@@ -101,7 +103,7 @@ describe('Typed Data', async () => {
         encryptionSk: ENC_SK,
         encryptionPubkey: ENC_PK,
       }),
-    { timeout: 20_000 }
+    { timeout: TIMEOUT_MS }
   )
 })
 
@@ -118,25 +120,27 @@ describe('Websocket Connection', () => {
         wsUrl,
       })
     },
-    { timeout: 20_000 }
+    { timeout: TIMEOUT_MS }
   )
 })
 
 describe('Seismic Precompiles', async () => {
-  test('RNG(1)', () => testRng({ chain, url }, 1), { timeout: 20_000 })
-  test('RNG(8)', () => testRng({ chain, url }, 8), { timeout: 20_000 })
-  test('RNG(16)', () => testRng({ chain, url }, 16), { timeout: 20_000 })
-  test('RNG(32)', () => testRng({ chain, url }, 32), { timeout: 20_000 })
+  test('RNG(1)', () => testRng({ chain, url }, 1), { timeout: TIMEOUT_MS })
+  test('RNG(8)', () => testRng({ chain, url }, 8), { timeout: TIMEOUT_MS })
+  test('RNG(16)', () => testRng({ chain, url }, 16), { timeout: TIMEOUT_MS })
+  test('RNG(32)', () => testRng({ chain, url }, 32), { timeout: TIMEOUT_MS })
   test('RNG(32, pers)', () => testRngWithPers({ chain, url }, 32), {
-    timeout: 20_000,
+    timeout: TIMEOUT_MS,
   })
-  test('ECDH', () => testEcdh({ chain, url }), { timeout: 20_000 })
+  test('ECDH', () => testEcdh({ chain, url }), { timeout: TIMEOUT_MS })
   test('HKDF(string)', () => testHkdfString({ chain, url }), {
-    timeout: 20_000,
+    timeout: TIMEOUT_MS,
   })
-  test('HKDF(hex)', () => testHkdfHex({ chain, url }), { timeout: 20_000 })
-  test('AES-GCM', () => testAesGcm({ chain, url }), { timeout: 20_000 })
-  test('secp256k1', () => testSecp256k1({ chain, url }), { timeout: 20_000 })
+  test('HKDF(hex)', () => testHkdfHex({ chain, url }), { timeout: TIMEOUT_MS })
+  test('AES-GCM', () => testAesGcm({ chain, url }), { timeout: TIMEOUT_MS })
+  test('secp256k1', () => testSecp256k1({ chain, url }), {
+    timeout: TIMEOUT_MS,
+  })
 })
 
 afterAll(async () => {
