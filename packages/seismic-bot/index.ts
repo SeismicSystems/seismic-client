824c175505c9386e459bcfc05eb7d36e25fa6a68
phexyz <32248504+phexyz@users.noreply.github.com>
2025-03-24 14:55:05 -0400

    last commit

--- packages/seismic-bot/index.ts
+++ packages/seismic-bot/index.ts
@@ -1,9 +1,10 @@
 import { checkAllFaucets } from '@sbot/faucetChecker'
-import { seismicDevnet1, seismicDevnet2 } from '@sviem/chain'
+import { seismicDevnet1, seismicDevnet2, seismicDevnet3 } from '@sviem/chain'
 
 const chains = [
   seismicDevnet1,
   seismicDevnet2,
+  seismicDevnet3,
   // createSeismicDevnet({ node: 3 }),
 ]
 checkAllFaucets(chains)
