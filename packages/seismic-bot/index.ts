c25ae53ee30ba9b3a8d199863a6debeb064c445d
cdrappi <c@seismic.systems>
2025-03-26 13:57:44 -0400

    one more faucet pk

--- packages/seismic-bot/index.ts
+++ packages/seismic-bot/index.ts
@@ -6,6 +6,7 @@ import { seismicDevnet1, seismicDevnet2, seismicDevnet3 } from '@sviem/chain'
 
 const FAUCET_PK_1 = process.env.FAUCET_1_PRIVATE_KEY! as Hex
 const FAUCET_PK_2 = process.env.FAUCET_2_PRIVATE_KEY! as Hex
+const FAUCET_PK_3 = process.env.FAUCET_3_PRIVATE_KEY! as Hex
 
 export type Faucets = Record<string, { chain: Chain; privateKeys: Hex[] }>
 
@@ -13,7 +14,7 @@ const faucets: Faucets = {
   'node-1': { chain: seismicDevnet1, privateKeys: [FAUCET_PK_1] },
   'node-2': {
     chain: seismicDevnet2,
-    privateKeys: [FAUCET_PK_1, FAUCET_PK_2],
+    privateKeys: [FAUCET_PK_1, FAUCET_PK_2, FAUCET_PK_3],
   },
   'node-3': { chain: seismicDevnet3, privateKeys: [FAUCET_PK_1] },
 }
