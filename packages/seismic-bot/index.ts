4bbb2b07a7253e3177628de733c5483cd98eeeea
Christian Drappi <christiandrappi@gmail.com>
2025-04-09 02:15:13 -0400

    ok

--- packages/seismic-bot/index.ts
+++ packages/seismic-bot/index.ts
@@ -8,8 +8,8 @@ const FAUCET_PK_1 = process.env.FAUCET_1_PRIVATE_KEY! as Hex
 const FAUCET_PK_2 = process.env.FAUCET_2_PRIVATE_KEY! as Hex
 const FAUCET_PK_3 = process.env.FAUCET_3_PRIVATE_KEY! as Hex
 
-const TRIVIA_ADMIN_ADDRESS = "0x124b6b46BC76115881367fC454c2212c1e7bc2Ac"
-const PUMP_DEPLOYER_ADDRESS = "0x000a2466401BE2B1090cB17fb51dD601C0642AFc"
+const TRIVIA_ADMIN_ADDRESS = '0x124b6b46BC76115881367fC454c2212c1e7bc2Ac'
+const PUMP_DEPLOYER_ADDRESS = '0x000a2466401BE2B1090cB17fb51dD601C0642AFc'
 
 export type Key = { pk: Hex; silent?: boolean }
 export type FaucetConfig = {
@@ -20,10 +20,10 @@ export type FaucetConfig = {
 export type Faucets = Record<string, FaucetConfig>
 
 const faucets: Faucets = {
-  'node-1': { 
+  'node-1': {
     chain: seismicDevnet1,
     privateKeys: [{ pk: FAUCET_PK_1 }],
-    extraAddresses: [TRIVIA_ADMIN_ADDRESS] 
+    extraAddresses: [TRIVIA_ADMIN_ADDRESS],
   },
   'node-2': {
     chain: seismicDevnet2,
