5c3a4f627b452885ad3e05285c4b3d1dba526db2
Christian Drappi <christiandrappi@gmail.com>
2025-04-09 02:08:00 -0400

    bump faucet amounts; add trivia/pump deployers

--- packages/seismic-bot/index.ts
+++ packages/seismic-bot/index.ts
@@ -1,4 +1,4 @@
-import { Chain } from 'viem'
+import { Address, Chain } from 'viem'
 import { Hex } from 'viem'
 
 import { checkAllFaucets } from '@sbot/faucetChecker'
@@ -8,15 +8,23 @@ const FAUCET_PK_1 = process.env.FAUCET_1_PRIVATE_KEY! as Hex
 const FAUCET_PK_2 = process.env.FAUCET_2_PRIVATE_KEY! as Hex
 const FAUCET_PK_3 = process.env.FAUCET_3_PRIVATE_KEY! as Hex
 
+const TRIVIA_ADMIN_ADDRESS = "0x124b6b46BC76115881367fC454c2212c1e7bc2Ac"
+const PUMP_DEPLOYER_ADDRESS = "0x000a2466401BE2B1090cB17fb51dD601C0642AFc"
+
 export type Key = { pk: Hex; silent?: boolean }
 export type FaucetConfig = {
   chain: Chain
   privateKeys: Key[]
+  extraAddresses: Address[]
 }
 export type Faucets = Record<string, FaucetConfig>
 
 const faucets: Faucets = {
-  'node-1': { chain: seismicDevnet1, privateKeys: [{ pk: FAUCET_PK_1 }] },
+  'node-1': { 
+    chain: seismicDevnet1,
+    privateKeys: [{ pk: FAUCET_PK_1 }],
+    extraAddresses: [TRIVIA_ADMIN_ADDRESS] 
+  },
   'node-2': {
     chain: seismicDevnet2,
     privateKeys: [
@@ -24,7 +32,12 @@ const faucets: Faucets = {
       { pk: FAUCET_PK_2 },
       { pk: FAUCET_PK_3, silent: true },
     ],
+    extraAddresses: [PUMP_DEPLOYER_ADDRESS],
   },
-  'node-3': { chain: seismicDevnet3, privateKeys: [{ pk: FAUCET_PK_1 }] },
+  'node-3': {
+    chain: seismicDevnet3,
+    privateKeys: [{ pk: FAUCET_PK_1 }],
+    extraAddresses: [],
+  },
 }
 checkAllFaucets(faucets)
