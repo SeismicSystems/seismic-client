24a623be34580c921a45467fb6384b9805162a2a
Christian Drappi <c@seismic.systems>
2025-05-09 14:04:30 -0400

    Make sure encryption nonce has no leading zeroes (#185)

--- packages/seismic-viem/src/crypto/nonce.ts
+++ packages/seismic-viem/src/crypto/nonce.ts
@@ -1,5 +1,24 @@
-import { Hex, bytesToHex } from 'viem'
+import { Hex, bytesToHex, trim } from 'viem'
 
 import { randomBytes } from '@noble/ciphers/webcrypto'
 
-export const randomEncryptionNonce = (): Hex => bytesToHex(randomBytes(12))
+export const randomEncryptionNonce = (): Hex => {
+  let nonce = bytesToHex(randomBytes(12))
+  while (nonce != trim(nonce)) {
+    /* 
+    TODO: this is a temporary fix.
+
+    RLP decoding fails because nonces are treated as uints and not bytes,
+    hence they cannot have leading zeroes.
+    
+    But we require exactly 12 bytes for the nonce
+
+    If nonce != trim(nonce), then the nonce has leading zeroes and is not valid.
+
+    This happens 1/256 times, so in that case we just try again
+    until it's fixed in (presumably) seismic-alloy
+    */
+    nonce = bytesToHex(randomBytes(12))
+  }
+  return nonce
+}
