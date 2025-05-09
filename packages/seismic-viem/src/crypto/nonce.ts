1e9b5ff8959b260d4bc5ca6d16d2dcf0d48142d3
cdrappi <c@seismic.systems>
2025-05-08 22:39:17 -0400

    trim encryption nonce bytes

--- packages/seismic-viem/src/crypto/nonce.ts
+++ packages/seismic-viem/src/crypto/nonce.ts
@@ -1,5 +1,6 @@
-import { Hex, bytesToHex } from 'viem'
+import { Hex, bytesToHex, trim } from 'viem'
 
 import { randomBytes } from '@noble/ciphers/webcrypto'
 
-export const randomEncryptionNonce = (): Hex => bytesToHex(randomBytes(12))
+export const randomEncryptionNonce = (): Hex =>
+  trim(bytesToHex(randomBytes(12)))
