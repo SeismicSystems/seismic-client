c049390f1f14caf40fcbc2d534b4142594a19b76
cdrappi <c@seismic.systems>
2025-07-17 11:04:41 -0400

    walletconnect is phased out

--- docs/sidebar.ts
+++ docs/sidebar.ts
@@ -182,11 +182,6 @@ const reactSidebar = [
         text: 'Privy',
         link: '/react/guides/privy',
       },
-      // TODO(lyron): Make walletconnect work
-      // {
-      //   text: 'WalletConnect',
-      //   link: '/react/guides/walletconnect',
-      // },
       // TODO(christian): Add AppKit guide
       // {
       //   text: 'AppKit',
