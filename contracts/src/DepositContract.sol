a6fcac781835bcf932a5d7b67dc9d94c8cb8439a
Ameya Deshmukh <ad@seismic.systems>
2025-11-11 22:32:34 +0530

    matthias updated contract

--- contracts/src/DepositContract.sol
+++ contracts/src/DepositContract.sol
@@ -9,25 +9,37 @@
 
 // SPDX-License-Identifier: CC0-1.0
 
-pragma solidity ^0.8.13;
+pragma solidity ^0.8.30;
 
 // This interface is designed to be compatible with the Vyper version.
 /// @notice This is the Ethereum 2.0 deposit contract interface.
 /// For more information see the Phase 0 specification under https://github.com/ethereum/eth2.0-specs
 interface IDepositContract {
     /// @notice A processed deposit event.
-    event DepositEvent(bytes pubkey, bytes withdrawal_credentials, bytes amount, bytes signature, bytes index);
+    event DepositEvent(
+        bytes node_pubkey,
+        bytes consensus_pubkey,
+        bytes withdrawal_credentials,
+        bytes amount,
+        bytes node_signature,
+        bytes consensus_signature,
+        bytes index
+    );
 
     /// @notice Submit a Phase 0 DepositData object.
-    /// @param pubkey A BLS12-381 public key.
+    /// @param node_pubkey An ED25519 public key.
+    /// @param consensus_pubkey A BLS12-381 public key.
     /// @param withdrawal_credentials Commitment to a public key for withdrawals.
-    /// @param signature A BLS12-381 signature.
+    /// @param node_signature An ED25519 signature.
+    /// @param consensus_signature A BLS12-381 signature.
     /// @param deposit_data_root The SHA-256 hash of the SSZ-encoded DepositData object.
     /// Used as a protection against malformed input.
     function deposit(
-        bytes calldata pubkey,
+        bytes calldata node_pubkey,
+        bytes calldata consensus_pubkey,
         bytes calldata withdrawal_credentials,
-        bytes calldata signature,
+        bytes calldata node_signature,
+        bytes calldata consensus_signature,
         bytes32 deposit_data_root
     ) external payable;
 
@@ -56,88 +68,99 @@ interface ERC165 {
 /// @notice This is the Ethereum 2.0 deposit contract interface.
 /// For more information see the Phase 0 specification under https://github.com/ethereum/eth2.0-specs
 contract DepositContract is IDepositContract, ERC165 {
-    uint256 constant DEPOSIT_CONTRACT_TREE_DEPTH = 32;
+    uint constant DEPOSIT_CONTRACT_TREE_DEPTH = 32;
     // NOTE: this also ensures `deposit_count` will fit into 64-bits
-    uint256 constant MAX_DEPOSIT_COUNT = 2 ** DEPOSIT_CONTRACT_TREE_DEPTH - 1;
+    uint constant MAX_DEPOSIT_COUNT = 2**DEPOSIT_CONTRACT_TREE_DEPTH - 1;
 
     bytes32[DEPOSIT_CONTRACT_TREE_DEPTH] branch;
     uint256 deposit_count;
 
     bytes32[DEPOSIT_CONTRACT_TREE_DEPTH] zero_hashes;
 
-    constructor() {
+    constructor() public {
         // Compute hashes in empty sparse Merkle tree
-        for (uint256 height = 0; height < DEPOSIT_CONTRACT_TREE_DEPTH - 1; height++) {
+        for (uint height = 0; height < DEPOSIT_CONTRACT_TREE_DEPTH - 1; height++)
             zero_hashes[height + 1] = sha256(abi.encodePacked(zero_hashes[height], zero_hashes[height]));
-        }
     }
 
-    function get_deposit_root() external view override returns (bytes32) {
+    function get_deposit_root() override external view returns (bytes32) {
         bytes32 node;
-        uint256 size = deposit_count;
-        for (uint256 height = 0; height < DEPOSIT_CONTRACT_TREE_DEPTH; height++) {
-            if ((size & 1) == 1) {
+        uint size = deposit_count;
+        for (uint height = 0; height < DEPOSIT_CONTRACT_TREE_DEPTH; height++) {
+            if ((size & 1) == 1)
                 node = sha256(abi.encodePacked(branch[height], node));
-            } else {
+            else
                 node = sha256(abi.encodePacked(node, zero_hashes[height]));
-            }
             size /= 2;
         }
-        return sha256(abi.encodePacked(node, to_little_endian_64(uint64(deposit_count)), bytes24(0)));
+        return sha256(abi.encodePacked(
+            node,
+            to_little_endian_64(uint64(deposit_count)),
+            bytes24(0)
+        ));
     }
 
-    function get_deposit_count() external view override returns (bytes memory) {
+    function get_deposit_count() override external view returns (bytes memory) {
         return to_little_endian_64(uint64(deposit_count));
     }
 
     function deposit(
-        bytes calldata pubkey,
+        bytes calldata node_pubkey,
+        bytes calldata consensus_pubkey,
         bytes calldata withdrawal_credentials,
-        bytes calldata signature,
+        bytes calldata node_signature,
+        bytes calldata consensus_signature,
         bytes32 deposit_data_root
-    ) external payable override {
+    ) override external payable {
         // Extended ABI length checks since dynamic types are used.
-        require(pubkey.length == 48, "DepositContract: invalid pubkey length");
+        require(node_pubkey.length == 32, "DepositContract: invalid node_pubkey length");
+        require(consensus_pubkey.length == 48, "DepositContract: invalid consensus_pubkey length");
         require(withdrawal_credentials.length == 32, "DepositContract: invalid withdrawal_credentials length");
-        require(signature.length == 96, "DepositContract: invalid signature length");
+        require(node_signature.length == 64, "DepositContract: invalid node_signature length");
+        require(consensus_signature.length == 96, "DepositContract: invalid consensus_signature length");
 
         // Check deposit amount
         require(msg.value >= 1 ether, "DepositContract: deposit value too low");
         require(msg.value % 1 gwei == 0, "DepositContract: deposit value not multiple of gwei");
-        uint256 deposit_amount = msg.value / 1 gwei;
+        uint deposit_amount = msg.value / 1 gwei;
         require(deposit_amount <= type(uint64).max, "DepositContract: deposit value too high");
 
         // Emit `DepositEvent` log
-        bytes memory amount = to_little_endian_64(uint64(deposit_amount));
-        emit DepositEvent(pubkey, withdrawal_credentials, amount, signature, to_little_endian_64(uint64(deposit_count)));
+          bytes memory amount = to_little_endian_64(uint64(deposit_amount));
+          emit DepositEvent(
+              node_pubkey,
+              consensus_pubkey,
+              withdrawal_credentials,
+              amount,
+              node_signature,
+              consensus_signature,
+              to_little_endian_64(uint64(deposit_count))
+          );
 
-        // Compute deposit data root (`DepositData` hash tree root)
-        bytes32 pubkey_root = sha256(abi.encodePacked(pubkey, bytes16(0)));
-        bytes32 signature_root = sha256(
-            abi.encodePacked(
-                sha256(abi.encodePacked(signature[:64])), sha256(abi.encodePacked(signature[64:], bytes32(0)))
-            )
-        );
-        bytes32 node = sha256(
-            abi.encodePacked(
-                sha256(abi.encodePacked(pubkey_root, withdrawal_credentials)),
-                sha256(abi.encodePacked(amount, bytes24(0), signature_root))
-            )
-        );
+          // Compute deposit data root (`DepositData` hash tree root)
+          bytes32 consensus_pubkey_hash = sha256(abi.encodePacked(consensus_pubkey, bytes16(0)));
+          bytes32 pubkey_root = sha256(abi.encodePacked(node_pubkey, consensus_pubkey_hash));
+          bytes32 node_signature_hash = sha256(node_signature);
+          bytes32 consensus_signature_hash = sha256(abi.encodePacked(
+              sha256(abi.encodePacked(consensus_signature[:64])),
+              sha256(abi.encodePacked(consensus_signature[64:], bytes32(0)))
+          ));
+          bytes32 signature_root = sha256(abi.encodePacked(node_signature_hash, consensus_signature_hash));
+          bytes32 node = sha256(abi.encodePacked(
+              sha256(abi.encodePacked(pubkey_root, withdrawal_credentials)),
+              sha256(abi.encodePacked(amount, bytes24(0), signature_root))
+          ));
 
         // Verify computed and expected deposit data roots match
-        require(
-            node == deposit_data_root,
-            "DepositContract: reconstructed DepositData does not match supplied deposit_data_root"
-        );
+        require(node == deposit_data_root, "DepositContract: reconstructed DepositData does not match supplied deposit_data_root");
 
         // Avoid overflowing the Merkle tree (and prevent edge case in computing `branch`)
         require(deposit_count < MAX_DEPOSIT_COUNT, "DepositContract: merkle tree full");
 
         // Add deposit data root to Merkle tree (update a single `branch` node)
         deposit_count += 1;
-        uint256 size = deposit_count;
-        for (uint256 height = 0; height < DEPOSIT_CONTRACT_TREE_DEPTH; height++) {
+        uint size = deposit_count;
+        for (uint height = 0; height < DEPOSIT_CONTRACT_TREE_DEPTH; height++) {
             if ((size & 1) == 1) {
                 branch[height] = node;
                 return;
@@ -150,7 +173,7 @@ contract DepositContract is IDepositContract, ERC165 {
         assert(false);
     }
 
-    function supportsInterface(bytes4 interfaceId) external pure override returns (bool) {
+    function supportsInterface(bytes4 interfaceId) override external pure returns (bool) {
         return interfaceId == type(ERC165).interfaceId || interfaceId == type(IDepositContract).interfaceId;
     }
 
