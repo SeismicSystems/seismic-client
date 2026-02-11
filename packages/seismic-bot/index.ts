f79da6e8539c462860af240a3c84737410ed551e
Christian Drappi <christiandrappi@gmail.com>
2026-02-11 12:24:31 -0500

    feat(faucet): set up seismic bot for internal-testnet

--- packages/seismic-bot/index.ts
+++ packages/seismic-bot/index.ts
@@ -2,7 +2,11 @@ import { Address, Chain } from 'viem'
 import { Hex } from 'viem'
 
 import { checkAllFaucets } from '@sbot/faucetChecker'
-import { seismicTestnet } from '@sviem/chain'
+import { createSeismicDevnet, seismicTestnet } from '@sviem/chain'
+
+const seismicInternalTestnet = createSeismicDevnet({
+  nodeHost: 'internal-1.seismictest.net',
+})
 
 const FAUCET_PK_1 = process.env.FAUCET_1_PRIVATE_KEY! as Hex
 const FAUCET_PK_2 = process.env.FAUCET_2_PRIVATE_KEY! as Hex
@@ -21,31 +25,34 @@ export type FaucetConfig = {
 }
 export type Faucets = Record<string, FaucetConfig>
 
+const getTestnetConfig = (chain: Chain): FaucetConfig => ({
+  chain,
+  privateKeys: [
+    { pk: FAUCET_PK_1 },
+    { pk: FAUCET_PK_2 },
+    { pk: FAUCET_PK_3, silent: true },
+  ],
+  extraAddresses: [
+    PUMP_DEPLOYER_ADDRESS,
+    POKER_DEPLOYER_ADDRESS,
+    POKER_RELAYER_ADDRESS,
+    '0x6D7E58BC9CB7e69117bDB3ccbE495560cDD2434F', // poker relayer 2
+    '0xdDBb6f358f290408D76847b4F602f0FD599295fd',
+    '0x478669bB3846d79F2fF511CE99eAEE8f85554476',
+    '0x8b2568E26Edcb132C8Ba7901be2924100C5155B9',
+    '0x88d213e2A577Bae274591fd5de5aC65F17b9881B', // terence
+    '0xB83c733772fA07Ed130F58099342416b72f6d9eD', // matt haines 1
+    '0xaa1cD3f5BCd5AeeA5F419c6c49a05F9E8Abc104B', // matt haines 2
+    '0xabbaeCb4698d59f1185321FA2c32710bC5bad9D3', // matthias
+    '0xaf97594412c1b7f2d2dc5abc2f5aeaef4162d4c2', // socialScan
+    '0x5B458bF97e86779927576Ea308Ea26FEE0b8a820', // tyler web3 technologies
+    '0x959807B8D94B324A74117956731F09E2893aCd72', // dalton
+    '0xfA82916B7c548e2D946351AA20dEAA1EEbdFd296', // christian studio desktop
+  ],
+})
+
 const faucets: Faucets = {
-  testnet: {
-    chain: seismicTestnet,
-    privateKeys: [
-      { pk: FAUCET_PK_1 },
-      { pk: FAUCET_PK_2 },
-      { pk: FAUCET_PK_3, silent: true },
-    ],
-    extraAddresses: [
-      PUMP_DEPLOYER_ADDRESS,
-      POKER_DEPLOYER_ADDRESS,
-      POKER_RELAYER_ADDRESS,
-      '0x6D7E58BC9CB7e69117bDB3ccbE495560cDD2434F', // poker relayer 2
-      '0xdDBb6f358f290408D76847b4F602f0FD599295fd',
-      '0x478669bB3846d79F2fF511CE99eAEE8f85554476',
-      '0x8b2568E26Edcb132C8Ba7901be2924100C5155B9',
-      '0x88d213e2A577Bae274591fd5de5aC65F17b9881B', // terence
-      '0xB83c733772fA07Ed130F58099342416b72f6d9eD', // matt haines 1
-      '0xaa1cD3f5BCd5AeeA5F419c6c49a05F9E8Abc104B', // matt haines 2
-      '0xabbaeCb4698d59f1185321FA2c32710bC5bad9D3', // matthias
-      '0xaf97594412c1b7f2d2dc5abc2f5aeaef4162d4c2', // socialScan
-      '0x5B458bF97e86779927576Ea308Ea26FEE0b8a820', // tyler web3 technologies
-      '0x959807B8D94B324A74117956731F09E2893aCd72', // dalton
-      '0xfA82916B7c548e2D946351AA20dEAA1EEbdFd296', // christian studio desktop
-    ],
-  },
+  testnet: getTestnetConfig(seismicTestnet),
+  'internal-testnet': getTestnetConfig(seismicInternalTestnet),
 }
 checkAllFaucets(faucets)
